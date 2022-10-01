b80d18c6466ceccdc2a243ec0a632f18b4cdb940
yunline <yunline@foxmail.com>
2022-10-01 11:04:45 +0800

    调整了前端的代码。增加了两只猫猫！

--- 前端/script/little_cat.js
+++ 前端/script/little_cat.js
@@ -1,12 +1,12 @@
 function set_column_count(){
     $("#cat_area").attr(
         "style",
-        `column-count:${Math.ceil(document.body.clientWidth/700)};
+        `column-count:${Math.ceil(document.body.clientWidth/800)};
         column-gap:1em;`
         );
 }
 
-function _callback(data, status) {
+function on_get(data, status) {
     if (status != "success") {
         console.log(status);
         return;
@@ -19,9 +19,7 @@ function _callback(data, status) {
         var cat_img_path = `../小猫/${cat}/${data[cat]["img"]}`;
         var _html = `
         <div class="cat_div">
-            <div class="cat_img">
-                <img src="${cat_img_path}" width=100%>
-            </div>
+            <img src="${cat_img_path}" class="cat_img">
             <div class="cat_text">
                 <h2>${cat}</h2>
                 <p>作者：${data[cat]["auther"] == "" ? "未知" : data[cat]["auther"]}</p>
@@ -30,13 +28,13 @@ function _callback(data, status) {
         </div>
         `;
         $("#cat_area").append(_html);
-
-        set_column_count();
     }
 }
+
 function on_ready() {
-    $.get("../小猫/index.json", _callback);
+    set_column_count();
+    $(window).resize(set_column_count);
+    $.get("../小猫/index.json", on_get);
 }
 
 $("document").ready(on_ready);
-$(window).resize(set_column_count);

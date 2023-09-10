2000b9eb19fe935e303b8d92306bac8aace90f88
yunline <yunline@foxmail.com>
2023-09-10 17:02:15 +0800

    format script with black

--- update_index.py
+++ update_index.py
@@ -2,87 +2,95 @@ import os
 import json
 import logging
 
-debug=0
+debug = 0
 
-logging.addLevelName(logging.INFO,"INFO ")
-logging.basicConfig(force=True,level=logging.DEBUG, format='[%(levelname)s] %(message)s',
-	datefmt='%Y-%d-%b %H:%M:%S')
+logging.addLevelName(logging.INFO, "INFO ")
+logging.basicConfig(
+    force=True,
+    level=logging.DEBUG,
+    format="[%(levelname)s] %(message)s",
+    datefmt="%Y-%d-%b %H:%M:%S",
+)
+
 
 def get_index():
-    index=[]
+    index = []
 
     for _path in os.listdir("cats"):
-        #遍历“小猫”文件夹，检查错误，获取目录。
+        # 遍历“小猫”文件夹，检查错误，获取目录。
 
-        path=os.path.join("cats",_path)
+        path = os.path.join("cats", _path)
 
         if not os.path.isdir(path):
-            #跳过非文件夹的路径
+            # 跳过非文件夹的路径
             continue
 
-        logging.info("Checking %s ···"%path)
+        logging.info("Checking %s ···" % path)
 
-        if not os.path.exists(os.path.join(path,"info.json")):
-            #如果没有info.json，报错
-            raise FileNotFoundError("\"info.json\" is not found at %s"%path)
+        if not os.path.exists(os.path.join(path, "info.json")):
+            # 如果没有info.json，报错
+            raise FileNotFoundError('"info.json" is not found at %s' % path)
 
-        with open(os.path.join(path,"info.json"),"r",encoding="utf8") as info_file:
-            #打开info.json
-            info=json.load(info_file)
+        with open(os.path.join(path, "info.json"), "r", encoding="utf8") as info_file:
+            # 打开info.json
+            info = json.load(info_file)
 
-        if not type(info)==dict:
-            #格式不对，不是dict，报错
-            raise Exception("Wrong format of \"info.json\". See ./README.md .")
+        if not type(info) == dict:
+            # 格式不对，不是dict，报错
+            raise Exception('Wrong format of "info.json". See ./README.md .')
 
-        for key in ["author","img","license","description"]:
+        for key in ["author", "img", "license", "description"]:
             if not key in info:
-                #缺少键，报错
-                raise KeyError("Key \"%s\" not found in \"info.json\"."%key)
-                
-            if not type(info[key])==str:
-                #不是字符串，报错
-                raise TypeError("Value of \"%s\" should be a string. (in \"info.json\")"%key)
+                # 缺少键，报错
+                raise KeyError('Key "%s" not found in "info.json".' % key)
 
-        if len(info["description"])>100:
+            if not type(info[key]) == str:
+                # 不是字符串，报错
+                raise TypeError(
+                    'Value of "%s" should be a string. (in "info.json")' % key
+                )
+
+        if len(info["description"]) > 100:
             raise Exception("Description is too long (more than 100 characters).")
 
-        img_path=os.path.join(path,info["img"])
+        img_path = os.path.join(path, info["img"])
         if not (os.path.exists(img_path) and os.path.isfile(img_path)):
-            #如果没有检测到图像文件，或者检测到的是文件夹，报错
-            raise FileNotFoundError("Image file \"%s\" not found."%img_path)
+            # 如果没有检测到图像文件，或者检测到的是文件夹，报错
+            raise FileNotFoundError('Image file "%s" not found.' % img_path)
 
-        #注意，脚本并不会检测图像文件的格式是否正确，需要审核者自己检查
+        # 注意，脚本并不会检测图像文件的格式是否正确，需要审核者自己检查
 
         logging.info("√")
 
         index.append(_path)
-    
+
     return index
 
+
 try:
-    index=get_index()
+    index = get_index()
 except Exception as err:
     if debug:
-        logging.error("",exc_info=err)
+        logging.error("", exc_info=err)
     else:
-        logging.error("%s: %s"%(type(err).__name__,err))
+        logging.error("%s: %s" % (type(err).__name__, err))
 else:
-    logging.info("There are %d images in total."%len(index))
+    logging.info("There are %d images in total." % len(index))
 
-    with open("templates/template_cat_count.svg","r",encoding="utf8") as template_f:
-        #读取svg模板
-        template=template_f.read()
-    
-    logging.info("Writing \"cat_count.svg\" ···")
+    with open("templates/template_cat_count.svg", "r", encoding="utf8") as template_f:
+        # 读取svg模板
+        template = template_f.read()
 
-    with open("data/cat_count.svg","w",encoding="utf8") as cat_count_f:
-        #写入cat_count.svg
-        cat_count_f.write(template.replace("{|CAT_COUNT|}",str(len(index))))
+    logging.info('Writing "cat_count.svg" ···')
 
-    logging.info("Writing \"index.json\" ···")
-    with open("data/index.json","w",encoding="utf8") as index_file:
+    with open("data/cat_count.svg", "w", encoding="utf8") as cat_count_f:
+        # 写入cat_count.svg
+        cat_count_f.write(template.replace("{|CAT_COUNT|}", str(len(index))))
+
+    logging.info('Writing "index.json" ···')
+    with open("data/index.json", "w", encoding="utf8") as index_file:
         # 写入index.json
-        json.dump(index,index_file,ensure_ascii=False)
+        json.dump(index, index_file, ensure_ascii=False)
     logging.info("Index updated successfully. ")
 finally:
     os.system("pause")
